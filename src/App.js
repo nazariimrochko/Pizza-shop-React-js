44702dde187e91ad1d3380a8e88f74af419350f0
Nazarii Mrochko <nazik409@gmail.com>
2022-02-03 17:22:37 +0200

    add DataBase (db.json ) create PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Header,} from "./componnents";
 import {MainContent,Cart} from "./pages";
 import {Routes,Route,} from "react-router-dom";
 
 function App() {
+    const [dataPizzas, setDataPizzas] = useState([])
+
+    useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+        .then((response) => response.json())
+        .then(json =>{setDataPizzas(json.pizzas)
+    })
+    },[])
+
+
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
                 <Routes>
-                    <Route path="/" element={<MainContent/>}  />
+                    <Route path="/"  element={ <MainContent itemPizza={dataPizzas}/>}  />
                     <Route path="/cart" element={<Cart/>}  />
                 </Routes>
             </div>
